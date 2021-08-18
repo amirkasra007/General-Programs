a578b77acff588b4b6d55bdfcccefc41a90bd11a
Amirkasra Amini <44532013+amirkasra007@users.noreply.github.com>
2021-08-18 11:24:10 +0200

    Update and rename dateConv.py to date Converter.py

--- date Converter.py
+++ date Converter.py
@@ -6,16 +6,19 @@ Mday = int(input ('Enter day: '))
 
 # Function to convert European date to Shamsi date
 def dateConv(Myear, Mmonth, Mday):
-    Mmonthlist = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-    Smonthlist = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29]
-#Checking 366-days year
+    Mmonthlist = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]    #Number of european Months days
+    Smonthlist = [31, 31, 31, 31, 31, 31, 30, 30, 30, 30, 30, 29]    #Number of solar Months days
+    
+#Checking 366-days year in european calendar
     if Myear%4 == 0:
         Mmonthlist[1] = 29
         
 #total number of days
     Mdays = (Myear - 1) * 365 + sum(Mmonthlist[0:Mmonth-1]) + Mday + (Myear -1)// 4
-    Sdays = Mdays -226899          #tafavot beyne avlin rooze miladi v shamsi
-    Syear = ((Sdays - (((Myear -1)//4) - 155) ) //365) + 1         #mohasebeye sale shamsi
+    
+    Sdays = Mdays -226899          #Difference between the first day of solar and European year
+    Syear = ((Sdays - (((Myear -1)//4) - 155) ) //365) + 1         #Calculating solar year
+    
     if ((Sdays - (((Myear -1)//4) - 155) ) %365)==0:
         Syear -= 1
         if Syear % 4 == 3:
@@ -25,9 +28,10 @@ def dateConv(Myear, Mmonth, Mday):
     else:
         Sdays = ((Sdays - (((Myear - 1) // 4) - 155)) % 365)
 
-    if  Syear %4 ==3:           #chek kardane sale kabise
+    if  Syear %4 ==3:           #Checking 366-days year in solar calendar
         Smonthlist[11] = 30
-#mohasebeye mah v rooz dar tarikhe shamsi
+#
+#Calculating the day and month in solar year
     i = 0
     Smonth = 1
 
@@ -38,7 +42,7 @@ def dateConv(Myear, Mmonth, Mday):
 
     Sday = Sdays
 
-    return [Syear, Smonth, Sday]          # khoroooji tabE
+    return [Syear, Smonth, Sday]          # output of the dateconv function
 
 
 [Syear, Smonth, Sday] = dateConv(Myear, Mmonth, Mday)
